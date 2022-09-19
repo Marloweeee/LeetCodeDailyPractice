3e8909b46c62cc083160774e37036e7cd43b032f
Marloweeee <convfactor@bupt.edu.cn>
2022-09-19 14:45:24 +0800

    9.19 first commit

---
+++ 2022.09/9.19-1636按照频率将数组升序排序.py
@@ -0,0 +1,6 @@
+from typing import List
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        cnt = Counter(nums)
+        nums.sort(key=lambda x: (cnt[x], -x))
+        return nums
